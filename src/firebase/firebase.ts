81b20a4b4f07cbd9834d54efde757a60897c181c
Astro-Syn <kelseyamyb@gmail.com>
2025-11-17 13:49:59 -0500

    change some stuff around

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth}  from 'firebase/auth';  // Add typing for Auth
+import { getFirestore, Firestore } from 'firebase/firestore';  // Add typing for Firestore
+import { getStorage } from 'firebase/storage';  // Add typing for Storage
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWkNsOvTFzkCGdfBzke5iiWMbvDXHEqek',
+  authDomain: 'driftergram-431d0.firebaseapp.com',
+  projectId: 'driftergram-431d0',
+  storageBucket: 'driftergram-431d0.firebasestorage.app',
+  messagingSenderId: '434353782950',
+  appId: '1:434353782950:web:9e60fc2b9c1047cb0bf467',
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export {app, auth, firestore, storage};
---
+++ src/hooks/useSignUpWithEmailAndPassword.ts
@@ -0,0 +1,70 @@
+
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase/firebase';
+import { doc, setDoc } from "firebase/firestore";
+import { firestore } from '../firebase/firebase';
+
+
+interface SignUpInputs {
+  email: string;
+  password: string;
+  username: string;
+  fullName: string;
+  
+}
+
+interface UseSignUpWithEmailAndPassword {
+  loading: boolean;
+  error: Error | null;
+  signup: (inputs: SignUpInputs) => Promise<void>;
+}
+
+
+const useSignUpWithEmailAndPassword = (): UseSignUpWithEmailAndPassword => {
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
+
+  const signup = async (inputs: SignUpInputs) => {
+    if (!inputs.email || !inputs.password || !inputs.username || !inputs.fullName) {
+      console.log("Please fill all the fields");
+      return;
+    }
+
+    try {
+      const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
+
+      if (error) {
+        console.log(error.message);
+        return;
+      }
+
+      if (newUser) {
+        const userDoc = {
+          uid: newUser.user.uid,
+          email: inputs.email,
+          username: inputs.username,
+          fullName: inputs.fullName,
+          bio: "",
+          profilePicURL: "",
+          followers: [],
+          following: [],
+          posts: [],
+          createdAt: Date.now(),
+        };
+
+        await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+        localStorage.setItem("user-info", JSON.stringify(userDoc));
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return { loading, error, signup };
+};
+
+export default useSignUpWithEmailAndPassword;
