36d5bdee5563737e5b71f51ec4dbdc37c440b009
Astro-Syn <kelseyamyb@gmail.com>
2025-11-25 13:33:53 -0500

    add some css

--- src/components/feedPosts/postFooter/PostFooter.tsx
+++ src/components/feedPosts/postFooter/PostFooter.tsx
@@ -1,59 +1,73 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import '../postFooter/PostFooter.css';
 import { FaHeart, FaRegHeart } from "react-icons/fa";
-import type PostHeader from '../postHeader/PostHeader';
 
 interface PostFooterProps {
   username: string;
   post: string;
 }
 
-
 const PostFooter: React.FC<PostFooterProps> = ({ username, post }) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(1000);
 
+  const [animate, setAnimate] = useState(false);
+
   const handleLike = () => {
-    if(liked){
+    setAnimate(true);
+    setTimeout(() => setAnimate(false), 400);
+
+    if (liked) {
       setLiked(false);
       setLikes(likes - 1);
-    }
-    else {
+    } else {
       setLiked(true);
       setLikes(likes + 1);
     }
-  }
-
+  };
 
   return (
     <div className='post-footer-container'>
+      <div className='like-section'>
+        <p className='likes'>{likes} likes</p>
+      
       <button
-      className='post-footer-like-status'
-      onClick={handleLike}
+        className='post-footer-like-status'
+        onClick={handleLike}
       >
-        {!liked ? (<FaHeart className='liked-heart' color='rgb(179, 214, 230)'/>) : (<FaRegHeart className='unlike-heart'/>)}
+        <span className="like-label">
+          {liked ? "Unlike" : "Like"}
+        </span>
+
+        {!liked ? (
+          <FaHeart 
+            className={`liked-heart ${animate ? "heart-pop" : ""}`} 
+          />
+        ) : (
+          <FaRegHeart 
+            className={`unlike-heart ${animate ? "heart-pop" : ""}`} 
+          />
+        )}
       </button>
 
+      </div>
+      
+
       <div className='post-comments-section'>
-        <p className='likes'>{likes} likes</p>
-        <p>
-          {username}
-        </p>
-        <p>
-          {post}
-        </p>
-        <span>
-          View all 1,500 comments
-        </span>
+        
+        <p>{username}</p>
+        <p>{post}</p>
+
+        <span>View all 1,500 comments</span>
+
         <div className='input-area'>
-          <input
-          placeholder='add a comment...'
-          />
+          <input placeholder='add a comment...' />
           <button>Post</button>
         </div>
       </div>
+
     </div>
-  )
+  );
 }
 
 export default PostFooter;
